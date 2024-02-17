f00e77505a77f794ce6c475e0ab7dd3efdf535d5
emonhosen0130 <irfanmuhammad013090@gmail.com>
2024-02-17 22:55:30 +0600

    valoee korci

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,8 +1,17 @@
-function play(){
+function play() {
     const homeSection = document.getElementById('home-screen');
     homeSection.classList.add('hidden')
     // console.log(homeSction)
 
     const playgroundSection = document.getElementById('play-ground');
     playgroundSection.classList.remove('hidden')
+}
+
+function continueGame() {
+    const alphabet = getRandomAlphabet();
+    console.log(alphabet);
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+    
+    setBackgroundColorById(alphabet);
 }
