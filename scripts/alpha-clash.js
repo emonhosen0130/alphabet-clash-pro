76b9d2e70e1826762e4cc5cba354113bc25ea280
emonhosen0130 <irfanmuhammad013090@gmail.com>
2024-02-17 21:35:26 +0600

    khalar mad tik korci

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,8 @@
+function play(){
+    const homeSection = document.getElementById('home-screen');
+    homeSection.classList.add('hidden')
+    // console.log(homeSction)
+
+    const playgroundSection = document.getElementById('play-ground');
+    playgroundSection.classList.remove('hidden')
+}
