8192acd6cf84d1383d26db762ad40a88d35b80fb
emonhosen0130 <irfanmuhammad013090@gmail.com>
2024-02-18 12:29:38 +0600

    almon ses ar pothe

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,6 +10,9 @@ function play() {
 function handleKeyboardButtonPress(event) {
     const PlayPressed = event.key;
     console.log('player pressed ', PlayPressed);
+    if(PlayPressed === 'Escape'){
+        gameOver();
+    }
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLowerCase();
@@ -17,12 +20,22 @@ function handleKeyboardButtonPress(event) {
 
     if (PlayPressed === expectedAlphabet) {
         console.log('You get a point');
+
+        const currentScore = getTextElementValueById('current-score');
+        console.log(currentScore);
+        const updateScore = currentScore + 1;
+        setTextElementValueById('current-score', updateScore);
+
+
+
         // console.log('you have pressed correctly', expectedAlphabet);
 
         // const currentScoreElement = document.getElementById('current-score');
         // const currentScoreText = currentScoreElement.innerText;
         // const currentScore = parseInt(currentScoreText);
         // console.log(currentScore);
+
+
         // const newScore = currentScore + 1;
         // currentScoreElement.innerText = newScore;
         removeBackgroundColorById(expectedAlphabet);
@@ -30,6 +43,16 @@ function handleKeyboardButtonPress(event) {
     }
     else {
         console.log('You lost a life');
+
+        const currentLife = getTextElementValueById('current-life');
+        const updateLife = currentLife - 1;
+        setTextElementValueById('current-life', updateLife); 
+
+
+        if(updateLife === 0 ){
+            gameOver();
+        }
+
         // const currentLifeElement = document.getElementById('current-life');
         // const currentLifeText = currentLifeElement.innerText;
         // const currentLife = parseInt(currentLifeText);
@@ -50,6 +73,19 @@ function continueGame() {
 
 function play() {
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
+    setTextElementValueById('current-life',5);
+    setTextElementValueById('current-score',0);
     continueGame()
 }
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+    const lastScore = getTextElementValueById('current-score');
+    console.log(lastScore);
+    setTextElementValueById('last-score', lastScore);
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
+} 
